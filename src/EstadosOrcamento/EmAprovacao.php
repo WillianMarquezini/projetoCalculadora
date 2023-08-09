0684b988192987b1ae9897296aee96b09275567c
José Willian Marquezine Valverde <josewillian.valverde@rbmweb.com.br>
2023-08-09 13:11:59 -0300

    adicionando estados

---
+++ src/EstadosOrcamento/EmAprovacao.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Alura\DesignPatterns\EstadosOrcamento;
+
+use Alura\DesignPatterns\Orcamento;
+
+class EmAprovacao extends EstadoOrcamento
+{
+    public function calculaDescontoExtra(Orcamento $orcamento): float
+    {
+        return $orcamento->valor * 0.5;
+    }
+
+    public function aprova(Orcamento $orcamento)
+    {
+        $orcamento->estadoAtual = new Aprovado();
+    }
+
+    public function reprova(Orcamento $orcamento)
+    {
+        $orcamento->estadoAtual = new Reprovado();
+    }
+}
--- src/Orcamento.php
+++ src/Orcamento.php
@@ -2,8 +2,36 @@
 
 namespace Alura\DesignPatterns;
 
+use Alura\DesignPatterns\EstadosOrcamento\EmAprovacao;
+use Alura\DesignPatterns\EstadosOrcamento\EstadoOrcamento;
+
 class Orcamento
 {
-    public float $valor;
     public int $quantidadeItens;
+    public float $valor;
+    public EstadoOrcamento $estadoAtual;
+
+    public function __construct()
+    {
+        $this->estadoAtual = new EmAprovacao();
+    }
+
+    private function calculaDescontoExtra()
+    {
+        $this->valor -= $this->estadoAtual->calculaDescontoExtra($this);
+    }
+
+    public function aprova()
+    {
+        $this->estadoAtual->aprova($this);
+    }
+    public function reprova()
+    {
+        $this->estadoAtual->reprova($this);
+    }
+    public function finaliza()
+    {
+        $this->estadoAtual->finaliza($this);
+    }
+
 }
